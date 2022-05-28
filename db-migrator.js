cf673370659ce8db3c693fc4ff0461fb0a455919
garbagemza <garbagemza@gmail.com>
2022-05-28 12:14:18 -0300

    redesigning database migration

--- db-migrator.js
+++ db-migrator.js
@@ -1,18 +1,36 @@
 
-const dbMigrator = function(requiredVersion, db, logger) {
+const dbMigrator = function(options) {
+    const db = options.database
+    const logger = options.logger
+    const targetVersion = options.targetVersion
+
     const currentVersion = currentDBVersion(db)
 
     logger(`npm-sqlite.dbmigrator.current.db.version:  ${currentVersion}`)
-    logger(`npm-sqlite.dbmigrator.required.db.version: ${requiredVersion}`)
+    logger(`npm-sqlite.dbmigrator.required.db.version: ${targetVersion}`)
 
-    for (let index = currentVersion; index < requiredVersion; index++) {
-        logger(`npm-sqlite.dbmigrator.migrating.db from ${index} to ${index + 1}`)
-        const transaction = db.transaction((stmts) => {
-            for (const stmt of stmts) stmt.run()
-        })
-        const statements = prepareStatements(index + 1, db)
-        transaction(statements)
-    }
+    // get required files
+    const requiredFiles = getRequiredFiles(currentVersion, targetVersion)
+    logger(`npm-sqlite.dbmigrator.required.files`)
+    requiredFiles.forEach(element => {
+        logger(` ${element}`)
+    });
+
+    // make sure the required files in workdir/migration/* exist
+    // load all migration files into memory
+    // split files into statements
+    // add target version statement
+    // execute transaction
+
+    // const transaction = db.transaction((stmts) => {
+    //     for (const stmt of stmts) stmt.run()
+    // })
+
+    // for (let index = currentVersion; index < targetVersion; index++) {
+    //     logger(`npm-sqlite.dbmigrator.migrating.db from ${index} to ${index + 1}`)
+    //     const statements = prepareStatements(index + 1, db)
+    //     transaction(statements)
+    // }
 }
 const prepareStatements = function(desiredVersion, db) {
     return [
--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const configure = function(options) {
     logger = options.verbose || function() {}
     logger(`npm-sqlite.configure ${JSON.stringify(options)}`)
     database = require('better-sqlite3')(`${options.workdir}/${options.databaseName}`, { verbose: logger })
-    dbMigrator(options.databaseVersion, database, logger)
+    
+    const migrationOptions = {
+        targetVersion: options.databaseVersion,
+        database: database,
+        logger: logger,
+        workdir: options.workdir
+    }
+    dbMigrator(migrationOptions)
 
 }
 
