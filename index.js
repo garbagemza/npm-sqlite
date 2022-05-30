8bb7a4b33fb62cd38bd40735fce35d8ea9077413
garbagemza <garbagemza@gmail.com>
2022-05-30 14:37:39 -0300

    extracted configure function

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-const dbMigrator = require('./db-migrator')
-
-var database = {}
-var logger = function() {}
-
-const configure = function(options) {
-    logger = options.verbose || function() {}
-    logger(`npm-sqlite.configure`)
-    logger(`-  workdir:         ${options.workdir}`)
-    logger(`-  migrationDir:    ${options.migrationDir}`)
-    logger(`-  databaseName:    ${options.databaseName}`)
-    logger(`-  databaseVersion: ${options.databaseVersion}`)
-
-    database = require('better-sqlite3')(`${options.workdir}/${options.databaseName}`, { verbose: logger })
-    
-    const migrationOptions = {
-        targetVersion: options.databaseVersion,
-        database: database,
-        logger: logger,
-        workdir: options.workdir,
-        migrationDir: options.migrationDir
-    }
-    dbMigrator(migrationOptions)
-    return database
-}
+const { configure } = require('./lib')
 
 module.exports = {
     configure: configure
