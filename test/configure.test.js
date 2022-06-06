1176b8c66fbfe22c6d09124508e58b3865a5a4e6
garbagemza <garbagemza@gmail.com>
2022-06-05 21:43:23 -0300

    #5 added coverage

--- test/configure.test.js
+++ test/configure.test.js
@@ -6,10 +6,11 @@
 
 const { configure } = require('../lib')
 const fs = require('fs')
-const { hasUncaughtExceptionCaptureCallback } = require('process')
 
 beforeAll(() => {
     fs.mkdirSync('./tmp')
+    fs.mkdirSync('./tmp/upgrade1')
+    fs.mkdirSync('./tmp/upgrade2')
 })
   
 afterAll(() => {
@@ -23,4 +24,29 @@ test('should create basic database', () => {
     }
     const t = () => configure(options)
     expect(t).not.toThrow()
+})
+
+test('should trigger db upgrade from 0', () => {
+    fs.writeFileSync('./tmp/upgrade1/0.sqlite', 'CREATE TABLE hello (id INTEGER PRIMARY KEY);')
+
+    const options = {
+        workdir: './tmp',
+        migrationDir: './tmp/upgrade1',
+        databaseName: 'upgrade',
+        databaseVersion: 1,
+    }
+    const t = () => configure(options)
+    expect(t).not.toThrow()
+})
+
+test('should not find upgrade files', () => {
+    const options = {
+        workdir: './tmp',
+        migrationDir: './tmp/upgrade2',
+        databaseName: 'lostmigration',
+        databaseVersion: 1,
+        verbose: console.log
+    }
+    const t = () => configure(options)
+    expect(t).toThrow()
 })
